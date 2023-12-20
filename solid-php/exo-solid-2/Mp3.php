62ed6940eab50a700a0981bb3b008a0e51ab1fba
Perrier-Richard <127929698+Perrier-Richard@users.noreply.github.com>
2023-12-20 11:10:47 +0100

    O as Open/Closed Principle

---
+++ solid-php/exo-solid-2/Mp3.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once("Music.php");
+
+class Mp3 extends Music
+{
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    public function listen()
+    {
+        return 'Lecture du fichier Mp3 '. $this->filename;
+    }
+}
---
+++ solid-php/exo-solid-2/Ogg.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once("Music.php");
+
+class Ogg extends Music
+{
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    public function listen()
+    {
+        return 'Lecture du fichier Ogg '. $this->filename;
+    }
+}
